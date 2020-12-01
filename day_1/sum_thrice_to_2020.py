ae1f83eb612828da508f232f3928eb650605d0de
Renée San Giorgi <renee@headfwd.com>
2020-12-01 14:45:50 +0100

    comment out print statements for much better performance :)

--- day_1/sum_thrice_to_2020.py
+++ day_1/sum_thrice_to_2020.py
@@ -2,21 +2,21 @@ def get_multiplication_of_entries_that_sum_to_2020(entries):
     number_of_entries = len(entries)
     number_of_loops = 0
     for i, entry in enumerate(entries):
-        print(f"new iteration for i: {i}")
+        # print(f"new iteration for i: {i}")
         
         for j in range(i + 1, number_of_entries):
-            print(f"new iteration for j: {j}")
+            # print(f"new iteration for j: {j}")
             for k in range(j + 1, number_of_entries):
                 number_of_loops +=1
-                print(f"new iteration for k: {k}")
-                print(f"""First number: {entry},
-                Second number: {entries[j]},
-                Third number: {entries[k]}""")
+                # print(f"new iteration for k: {k}")
+                # print(f"""First number: {entry},
+                # Second number: {entries[j]},
+                # Third number: {entries[k]}""")
                 entry_sum = entry + entries[j] + entries[k]
-                print(f"sum is: {entry_sum}")
+                # print(f"sum is: {entry_sum}")
                 if entry_sum == 2020:
-                    print(f"found it! {entry}, {entries[j]}, {entries[k]}")
-                    print(f"# of tries: {number_of_loops}")
+                    # print(f"found it! {entry}, {entries[j]}, {entries[k]}")
+                    # print(f"# of tries: {number_of_loops}")
                     return entry * entries[j] * entries[k]
 
 def data_to_list(file_path):
