252aac33a4f27cb6b0e8d6c747c07d2a9858c4d2
Renée San Giorgi <renee@headfwd.com>
2020-12-05 09:15:25 +0100

    solved 1

--- day_5/get_highest_seatID.py
+++ day_5/get_highest_seatID.py
@@ -1,35 +1,43 @@
 from functools import reduce
 
 def get_highest_seat_id(boarding_passes):
+    seat_ids = []
     for boarding_pass in boarding_passes:
-        print(f"boarding pass: {boarding_pass}")
-        print(get_row(boarding_pass))
-        print(get_column(boarding_pass))
+        seat_id = get_seat_id(boarding_pass)
+        seat_ids.append(seat_id)
+        print(f"boarding pass: {boarding_pass}, seat id: {seat_id}")
+    return max(seat_ids)
 
-def get_column(boarding_pass):
-    column_info = boarding_pass[7:]
-    print(column_info)
-    possible_columns = list(range(0, 8))
-    for i in range(3):
-        if column_info[i] == "R":
-            possible_columns = get_remaining_half("UPPER", possible_columns)
-        elif column_info[i] == "L":
-            possible_columns = get_remaining_half("LOWER", possible_columns)
-    return possible_columns[0]
 
-def get_row(boarding_pass):
-    row_info = boarding_pass[:-3]
-    print(row_info)
-    return row_info
-
-def get_seat_id(row, column):
+def get_seat_id(boarding_pass):
+    row = get_place_info(boarding_pass, "ROW")
+    column = get_place_info(boarding_pass, "COLUMN")
     return row * 8 + column
 
-def get_remaining_half(mode, full_range):
-    half_range = int(len(full_range)*0.5)
-    if mode == "LOWER":
+def get_place_info(boarding_pass, mode):
+    ticket_info = ""
+    possibilities = []
+    if mode == "ROW":
+        ticket_info = boarding_pass[:-3]
+        possibilities = list(range(128))
+    elif mode == "COLUMN":
+        ticket_info = boarding_pass[7:]
+        possibilities = list(range(8))
+    for i in range(len(ticket_info)):
+        possibilities = get_half_possibilities(ticket_info[i], possibilities)
+    return possibilities[0]
+
+def get_half_possibilities(place_indicator, remaining_possibilities):
+    if place_indicator in ["R", "B"]:
+        return split_range("UPPER", remaining_possibilities)
+    if place_indicator in ["L", "F"]:
+        return split_range("LOWER", remaining_possibilities)
+
+def split_range(side_indicator, full_range):
+    half_range = len(full_range) // 2
+    if side_indicator == "LOWER":
         return full_range[:-half_range]
-    elif mode == "UPPER":
+    if side_indicator == "UPPER":
         return full_range[half_range:]
 
 def data_to_list(file_path):
@@ -38,5 +46,4 @@ def data_to_list(file_path):
     return list(map(str, content.split()))
 
 if __name__ == "__main__":
-    # print(get_remaining_half("LOWER", [1,2,3,4]))
-    get_highest_seat_id(data_to_list("./data"))
+    print(get_highest_seat_id(data_to_list("./data")))
