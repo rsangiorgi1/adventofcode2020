aa1f0fdffb9e13b821ffa7e6e3e4913716be19b5
Renée San Giorgi <renee@headfwd.com>
2020-12-06 08:40:16 +0100

    solved 1

---
+++ day_6/get_sum_any.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+def get_sum(answers):
+    counted_answers = list(map(lambda x: len(set(x)), answers))
+    print(counted_answers)
+    return reduce(lambda x, y: x + y, counted_answers)
+
+def data_to_list(file_path):
+    datafile = open(file_path)
+    content = datafile.read()
+    grouped_content = list(map(str, content.split("\n\n")))
+    return list(map(lambda x: x.replace("\n", ""), grouped_content))
+
+if __name__ == "__main__":
+    print(get_sum(data_to_list("./data")))
